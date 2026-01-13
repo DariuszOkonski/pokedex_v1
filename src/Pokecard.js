97512b33284b6b7722709068eb077eff53801fd1
Dariusz Okoński <darek200180@gmail.com>
2026-01-13 07:12:19 +0100

    add logic

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -14,7 +14,7 @@ class Pokecard extends Component {
         <h1 className='Pokecard-title'>{this.props.name}</h1>
         <img className='Pokecard-img' src={imgSrc} alt={this.props.name} />
         <div className='Pokecard-data'>Type: {this.props.type}</div>
-        <div className='Pokecard-data'>Exp: {this.props.experience}</div>
+        <div className='Pokecard-data'>Exp: {this.props.exp}</div>
       </div>
     );
   }
